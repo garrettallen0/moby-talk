4ff09965a52f067f6b02a8ed04049925e889fa6b
garrettallen0 <garrett.allen13@gmail.com>
2025-06-16 17:16:26 -0700

    add navbar and icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import { AuthButton } from './components/AuthButton';
+import { Navbar } from './components/Navbar';
 import { Home } from './components/Home';
 import { MapDetail } from './components/MapDetail';
 import { MapEditor } from './components/MapEditor';
@@ -8,16 +8,18 @@ import { MapEditor } from './components/MapEditor';
 function App() {
   return (
     <Router>
-      <div className="container">
-        <AuthButton />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/map">
-            <Route index element={<MapDetail />} />
-            <Route path="new" element={<MapEditor />} />
-            <Route path=":id/edit" element={<MapEditor />} />
-          </Route>
-        </Routes>
+      <div className="app">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/map">
+              <Route index element={<MapDetail />} />
+              <Route path="new" element={<MapEditor />} />
+              <Route path=":id/edit" element={<MapEditor />} />
+            </Route>
+          </Routes>
+        </main>
       </div>
     </Router>
   );
