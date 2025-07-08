09aa10e1089b64dc55c61e1dfb1edcf7faf90a9a
garrettallen0 <garrett.allen13@gmail.com>
2025-07-08 16:23:55 -0700

    add config for tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--primary-color)',
+        border: 'var(--border-color)',
+        text: 'var(--text-color)',
+        'text-secondary': 'var(--text-secondary)',
+        background: 'var(--background-color)',
+      }
+    },
   },
   plugins: [],
 }
