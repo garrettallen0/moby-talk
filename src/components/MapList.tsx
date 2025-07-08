39b467bebc5b5e4134c16a17d0cb82b61c45bb41
garrettallen0 <garrett.allen13@gmail.com>
2025-07-08 16:58:13 -0700

    style MapCard

--- src/components/MapList.tsx
+++ src/components/MapList.tsx
@@ -106,7 +106,7 @@ export const MapList = ({
         <div>
           {maps.length > 0 ? (
             <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 p-4 md:p-2">
-              {maps.map((map) => (
+              {maps.map((map, index) => (
                 <MapCard
                   key={map.id}
                   map={map}
@@ -115,6 +115,7 @@ export const MapList = ({
                   onComment={handleComment}
                   onDelete={handleDelete}
                   showDelete={showDelete}
+                  index={index}
                 />
               ))}
             </div>
